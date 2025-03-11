d45ea3f579bd639985f4542cabdefb77351a9218
Phillip Pilgrim <filipmusalek.osobni@gmail.com>
2025-03-11 17:32:00 +0100

    dga

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function NotFound() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-6xl">404</h1>
+        </div>
+    );
+}
