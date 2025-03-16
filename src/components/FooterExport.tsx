b90751da770c4ba8305f4a016adc1b58f2382f82
Phillip Pilgrim <filipmusalek.osobni@gmail.com>
2025-03-16 15:04:06 +0100

    Update 0.6.0

--- src/components/FooterExport.tsx
+++ src/components/FooterExport.tsx
@@ -1,121 +1,122 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { PoppinsSemiBold } from "@/lib/fonts";
-import { EMAIL, CISLO } from "@/utils/constant";
 import { IoLogoFacebook, IoBookmarks } from "react-icons/io5";
+import { EMAIL, CISLO } from "@/utils/constant";
 import { useTranslation } from "react-i18next";
+import { PoppinsSemiBold } from "@/lib/fonts";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 
 export default function FooterExport() {
-  const { t } = useTranslation();
-  const [mounted, setMounted] = useState(false);
+	const { t } = useTranslation();
+	const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+	useEffect(() => {
+		setMounted(true);
+	}, []);
 
-  if (!mounted) return null;
+	if (!mounted) return null;
 
-  return (
-    <footer className={`bg-[#f5f5f5] py-4 md:px-0 px-4 shadow-md ${PoppinsSemiBold.className}`}>
-      <div className="container mx-auto flex flex-col items-start justify-between gap-6 p-0 md:flex-row md:p-4">
-        <div className="flex-1">
-          <p className="text-md mb-2 font-semibold">{t("footer.watch")}</p>
-          <div className="flex flex-col gap-2">
-            <a
-              href="https://www.facebook.com/profile.php?id=61558579918001"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-2 transition-all duration-300 hover:scale-105 hover:text-[#0866ff]"
-            >
-              <IoLogoFacebook className="text-2xl" />
-              <span className="text-md">Facebook</span>
-            </a>
-            <a
-              href="https://www.booking.com/Share-mz7jvW"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-2 transition-all duration-300 hover:scale-105 hover:text-[#003993]"
-            >
-              <IoBookmarks className="text-2xl" />
-              <span className="text-md">Booking.com</span>
-            </a>
-          </div>
-          <div className="mt-8 flex-1">
-            <a
-              href="https://phillippilgrim.xyz/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-md transition-all hover:scale-105 hover:text-[#e94629]"
-            >
-              {t("footer.made")}
-            </a>
-          </div>
-        </div>
+	return (
+		<footer className={`bg-[#f5f5f5] px-4 py-4 shadow-md md:px-0 ${PoppinsSemiBold.className}`}>
+			<div className="container mx-auto flex flex-col items-start justify-between gap-6 p-0 md:flex-row md:p-4">
+				<div className="flex-1">
+					<p className="text-md mb-2 font-semibold">{t("footer.watch")}</p>
+					<div className="flex flex-col gap-2">
+						<a
+							href="https://www.facebook.com/profile.php?id=61558579918001"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="flex items-center gap-2 transition-all duration-300 hover:scale-105 hover:text-[#0866ff]"
+						>
+							<IoLogoFacebook className="text-2xl" />
+							<span className="text-md">Facebook</span>
+						</a>
+						<a
+							href="https://www.booking.com/Share-mz7jvW"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="flex items-center gap-2 transition-all duration-300 hover:scale-105 hover:text-[#003993]"
+						>
+							<IoBookmarks className="text-2xl" />
+							<span className="text-md">Booking.com</span>
+						</a>
+					</div>
+					<div className="mt-8 flex-1">
+						<a
+							href="https://phillippilgrim.xyz/"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="text-md transition-all hover:scale-105 hover:text-[#e94629]"
+						>
+							{t("footer.made")}
+						</a>
+					</div>
+				</div>
 
-        <div className="flex-1">
-          <p className="text-md mb-2 font-semibold">{t("footer.kontakt")}</p>
-          <div className="flex flex-col gap-1">
-            <a
-              href={`mailto:${EMAIL}`}
-              className="text-md text-zinc-800 transition-all hover:text-[#e94629]"
-            >
-              • {EMAIL}
-            </a>
-            <a
-              href={`tel:${CISLO}`}
-              className="text-md text-zinc-800 transition-all hover:text-[#e94629]"
-            >
-              • {CISLO}
-            </a>
-            <p className="text-md text-zinc-800"> • Ostravská 83, Bohumín, Vrbice</p>
-            <p className="text-md text-zinc-800"> • Ban.účet 1761182042/3030</p>
-          </div>
-        </div>
+				<div className="flex-1">
+					<p className="text-md mb-2 font-semibold">{t("footer.kontakt")}</p>
+					<div className="flex flex-col gap-1">
+						<a
+							href={`mailto:${EMAIL}`}
+							className="text-md text-zinc-800 transition-all hover:text-[#e94629]"
+						>
+							• {EMAIL}
+						</a>
+						<a href={`tel:${CISLO}`} className="text-md text-zinc-800 transition-all hover:text-[#e94629]">
+							• {CISLO}
+						</a>
+						<p className="text-md text-zinc-800"> • Ostravská 83, Bohumín, Vrbice</p>
+						<p className="text-md text-zinc-800"> • Ban.účet 1761182042/3030</p>
+					</div>
+				</div>
 
-        <div className="flex-1">
-          <p className="text-md mb-2 font-semibold">{t("footer.partners")}</p>
-          <div className="flex flex-wrap gap-4">
-            <a href="https://www.ostrava.cz/cs" target="_blank" rel="noopener noreferrer">
-              <Image
-                src="/assets/ostrava!!!.png"
-                alt="Logo partnera"
-                width={120}
-                height={120}
-                className="cursor-pointer transition-transform hover:scale-105"
-              />
-            </a>
-            <a href="https://www.kudyznudy.cz/" target="_blank" rel="noopener noreferrer">
-              <Image
-                src="/assets/kudyznudy.png"
-                alt="Logo partnera"
-                width={120}
-                height={120}
-                className="cursor-pointer transition-transform hover:scale-105"
-              />
-            </a>
-            <a href="https://www.tesinskeslezsko.cz/" target="_blank" rel="noopener noreferrer">
-              <Image
-                src="/assets/tesinskeslzesko.png"
-                alt="Logo partnera"
-                width={120}
-                height={120}
-                className="cursor-pointer transition-transform hover:scale-105"
-              />
-            </a>
-            <a href="https://www.navsi.cz/aktualne/aktuality/slevove-karty-tesinske-slezsko-region-card-2024-1585cs.html" target="_blank" rel="noopener noreferrer">
-              <Image
-                src="/assets/tesinskekarta.png"
-                alt="Logo partnera"
-                width={120}
-                height={120}
-                className="cursor-pointer transition-transform hover:scale-105"
-              />
-            </a>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+				<div className="flex-1">
+					<p className="text-md mb-2 font-semibold">{t("footer.partners")}</p>
+					<div className="flex flex-wrap gap-4">
+						<a href="https://www.ostrava.cz/cs" target="_blank" rel="noopener noreferrer">
+							<Image
+								src="/assets/ostrava!!!.png"
+								alt="Logo partnera"
+								width={120}
+								height={120}
+								className="cursor-pointer transition-transform hover:scale-105"
+							/>
+						</a>
+						<a href="https://www.kudyznudy.cz/" target="_blank" rel="noopener noreferrer">
+							<Image
+								src="/assets/kudyznudy.png"
+								alt="Logo partnera"
+								width={120}
+								height={120}
+								className="cursor-pointer transition-transform hover:scale-105"
+							/>
+						</a>
+						<a href="https://www.tesinskeslezsko.cz/" target="_blank" rel="noopener noreferrer">
+							<Image
+								src="/assets/tesinskeslzesko.png"
+								alt="Logo partnera"
+								width={120}
+								height={120}
+								className="cursor-pointer transition-transform hover:scale-105"
+							/>
+						</a>
+						<a
+							href="https://www.navsi.cz/aktualne/aktuality/slevove-karty-tesinske-slezsko-region-card-2024-1585cs.html"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Image
+								src="/assets/tesinskekarta.png"
+								alt="Logo partnera"
+								width={120}
+								height={120}
+								className="cursor-pointer transition-transform hover:scale-105"
+							/>
+						</a>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
 }
--- src/components/NavbarExport.tsx
+++ src/components/NavbarExport.tsx
@@ -13,13 +13,14 @@ import { AnimatePresence } from "motion/react";
 import Image from "next/image";
 
 export default function NavbarExport() {
-	const [mounted, setMounted] = useState(false);
-	const { scrollY } = useScroll();
-	const opacity = useTransform(scrollY, [0, 100], [1, 0]);
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 	const dropdownRef = useRef<HTMLDivElement>(null);
+	const [mounted, setMounted] = useState(false);
 	const [isOpen, setIsOpen] = useState(false);
 	const { t, i18n } = useTranslation();
-	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+	const { scrollY } = useScroll();
+	const opacity = useTransform(scrollY, [0, 100], [1, 0]);
 
 	const toggleMobileMenu = () => {
 		setIsMobileMenuOpen(prev => !prev);
@@ -51,7 +52,6 @@ export default function NavbarExport() {
 
 	return (
 		<>
-			{/* Desktop Navbar */}
 			<motion.nav
 				style={{ opacity }}
 				className={`fixed top-0 left-0 z-20 hidden w-full px-16 py-4 lg:block ${PoppinsMedium.className}`}
@@ -70,7 +70,10 @@ export default function NavbarExport() {
 					</motion.a>
 
 					<div ref={dropdownRef} className="relative flex w-full items-center justify-center lg:w-auto">
-						<span
+						<motion.span
+							initial={{ opacity: 0, x: 30 }}
+							animate={{ opacity: 1, x: 0 }}
+							transition={{ duration: 0.5, delay: 0.4 }}
 							onClick={toggleDropdown}
 							aria-expanded={isOpen}
 							className="flex w-40 cursor-pointer items-center px-4 py-2 font-[family-name:var(--font-coahce-regular)] text-sm font-bold text-[#e94629] lg:text-base"
@@ -86,7 +89,7 @@ export default function NavbarExport() {
 							>
 								<IoArrowDown />
 							</motion.div>
-						</span>
+						</motion.span>
 
 						<AnimatePresence>
 							{isOpen && (
@@ -171,133 +174,136 @@ export default function NavbarExport() {
 				</div>
 			</motion.nav>
 
-			{/* Mobile Navbar Toggle Button */}
 			<motion.nav style={{ opacity }} className="fixed top-0 left-0 z-20 w-full px-4 py-4 lg:hidden">
-				<button
+				<motion.button
+					initial={{ opacity: 0, x: 30 }}
+					animate={{ opacity: 1, x: 0 }}
+					transition={{ duration: 0.5, delay: 0.4 }}
 					onClick={toggleMobileMenu}
 					aria-label="Open Mobile Menu"
 					className={`fixed top-3 right-1 z-20 px-4 py-4 lg:hidden ${isMobileMenuOpen ? "hidden" : ""}`}
 				>
 					<IoMenu className="text-[#e94629]" size={30} />
-				</button>
+				</motion.button>
 
-				{/* Language Selector */}
-				<Link href="/" className={`flex items-center gap-2 ${isMobileMenuOpen ? "hidden" : ""}`}>
-					<Image src="/assets/logo.png" alt="Logo" width={200} height={200} />
-				</Link>
+				<motion.div
+					initial={{ opacity: 0, x: -30 }}
+					animate={{ opacity: 1, x: 0 }}
+					transition={{ duration: 0.5, delay: 0.4 }}
+				>
+					<Link href="/" className={`flex items-center gap-2 ${isMobileMenuOpen ? "hidden" : ""}`}>
+						<Image src="/assets/logo.png" alt="Logo" width={200} height={200} />
+					</Link>
+				</motion.div>
 			</motion.nav>
 
-			{/* Mobile Menu */}
 			<AnimatePresence>
-  {isMobileMenuOpen && (
-    <motion.div
-      initial={{ opacity: 0, y: "-100%" }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: "-100%" }}
-      transition={{ duration: 0.3 }}
-      className="bg-primary-background fixed inset-x-0 top-0 z-50 h-screen w-full bg-black/90"
-    >
-      <div className="flex h-full flex-col px-4 py-6">
-        <div className="flex items-center justify-between">
-          <Link href="/" className="flex flex-row items-center gap-2">
-            <Image src="/assets/logo.png" alt="Logo" width={200} height={200} />
-          </Link>
+				{isMobileMenuOpen && (
+					<motion.div
+						initial={{ opacity: 0, y: "-100%" }}
+						animate={{ opacity: 1, y: 0 }}
+						exit={{ opacity: 0, y: "-100%" }}
+						transition={{ duration: 0.3 }}
+						className="bg-primary-background fixed inset-x-0 top-0 z-50 h-screen w-full bg-black/90"
+					>
+						<div className="flex h-full flex-col px-4 py-6">
+							<div className="flex items-center justify-between">
+								<Link href="/" className="flex flex-row items-center gap-2">
+									<Image src="/assets/logo.png" alt="Logo" width={200} height={200} />
+								</Link>
 
-          <button onClick={toggleMobileMenu}>
-            <IoClose className="absolute top-8 right-4 text-white" size={36} />
-            <span className="sr-only">Close Menu</span>
-          </button>
-        </div>
+								<button onClick={toggleMobileMenu}>
+									<IoClose className="absolute top-8 right-4 text-white" size={36} />
+									<span className="sr-only">Close Menu</span>
+								</button>
+							</div>
 
-        <div className="mt-10">
-          <div className="flex flex-col gap-4">
-            <motion.div
-              initial={{ opacity: 0, x: -30 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.5, delay: 0.2 }}
-            >
-              <Link
-                href="/apartmany"
-                className="mb-1.5 font-[family-name:var(--font-generalsans-medium)] text-2xl text-white uppercase transition-transform duration-300 focus:scale-110"
-                onClick={toggleMobileMenu}
-              >
-                {t("navbar.home")}
-              </Link>
-            </motion.div>
-            <motion.div
-              initial={{ opacity: 0, x: -30 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.5, delay: 0.5 }}
-            >
-              <Link
-                href="https://www.booking.com/hotel/cz/apartma-vrbice.cs.html"
-                rel="noreferrer noopener"
-                target="_blank"
-                className="mb-1.5 font-[family-name:var(--font-generalsans-medium)] text-2xl text-white uppercase transition-transform duration-300 focus:scale-110"
-                onClick={toggleMobileMenu}
-              >
-                {t("navbar.rezervace")}
-              </Link>
-            </motion.div>
-          </div>
-        </div>
+							<div className="mt-10">
+								<div className="flex flex-col gap-4">
+									<motion.div
+										initial={{ opacity: 0, x: -30 }}
+										animate={{ opacity: 1, x: 0 }}
+										transition={{ duration: 0.5, delay: 0.2 }}
+									>
+										<Link
+											href="/apartmany"
+											className="mb-1.5 font-[family-name:var(--font-generalsans-medium)] text-2xl text-white uppercase transition-transform duration-300 focus:scale-110"
+											onClick={toggleMobileMenu}
+										>
+											{t("navbar.home")}
+										</Link>
+									</motion.div>
+									<motion.div
+										initial={{ opacity: 0, x: -30 }}
+										animate={{ opacity: 1, x: 0 }}
+										transition={{ duration: 0.5, delay: 0.5 }}
+									>
+										<Link
+											href="https://www.booking.com/hotel/cz/apartma-vrbice.cs.html"
+											rel="noreferrer noopener"
+											target="_blank"
+											className="mb-1.5 font-[family-name:var(--font-generalsans-medium)] text-2xl text-white uppercase transition-transform duration-300 focus:scale-110"
+											onClick={toggleMobileMenu}
+										>
+											{t("navbar.rezervace")}
+										</Link>
+									</motion.div>
+								</div>
+							</div>
 
-        {/* Obalový kontejner pro jazykové ikonky a tlačítko s telefonem */}
-        <div className="mt-auto flex flex-col">
-          <div className="flex flex-row justify-between">
-            <motion.button
-              initial={{ opacity: 0, x: -30 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.5, delay: 0.7 }}
-              onClick={() => changeLanguage("cs")}
-              className="flex cursor-pointer items-center justify-start gap-2 duration-300 focus:scale-125"
-            >
-              <Image
-                src={"/assets/czech-flag.png"}
-                alt="Czech"
-                width={55}
-                height={55}
-                className="opacity-80"
-              />
-            </motion.button>
+							<div className="mt-auto flex flex-col">
+								<div className="flex flex-row justify-between">
+									<motion.button
+										initial={{ opacity: 0, x: -30 }}
+										animate={{ opacity: 1, x: 0 }}
+										transition={{ duration: 0.5, delay: 0.7 }}
+										onClick={() => changeLanguage("cs")}
+										className="flex cursor-pointer items-center justify-start gap-2 transition-none focus:duration-300 focus:transition-transform focus:scale-115"
+									>
+										<Image
+											src={"/assets/czech-flag.png"}
+											alt="Czech"
+											width={55}
+											height={55}
+											className="opacity-80"
+										/>
+									</motion.button>
 
-            <motion.button
-              initial={{ opacity: 0, x: 30 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.5, delay: 0.7 }}
-              onClick={() => changeLanguage("en_us")}
-              className="flex cursor-pointer items-center justify-start gap-2 duration-300 focus:scale-125"
-            >
-              <Image
-                src={"/assets/english-flag.png"}
-                alt="English"
-                width={55}
-                height={55}
-                className="opacity-80"
-              />
-            </motion.button>
-          </div>
+									<motion.button
+										initial={{ opacity: 0, x: 30 }}
+										animate={{ opacity: 1, x: 0 }}
+										transition={{ duration: 0.5, delay: 0.7 }}
+										onClick={() => changeLanguage("en_us")}
+										className="flex cursor-pointer items-center justify-start gap-2 transition-none focus:duration-300 focus:transition-transform focus:scale-115"
+									>
+										<Image
+											src={"/assets/english-flag.png"}
+											alt="English"
+											width={55}
+											height={55}
+											className="opacity-80"
+										/>
+									</motion.button>
+								</div>
 
-          {/* Telefonní tlačítko, posunuté mírně dolů */}
-          <div className="mx-auto mt-4 w-full">
-            <motion.a
-              initial={{ opacity: 0, y: 30 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: 1 }}
-              whileHover={{ scale: 1.1, transition: { duration: 0.3 } }}
-              href={`tel:${CISLO}`}
-              className="flex w-full items-center justify-center rounded-lg border-2 border-[#e94629] bg-black/70 p-1 text-[#e94629] focus:scale-110"
-            >
-              <IoCallOutline className="p-1 text-4xl" />
-              <span className="ml-2 text-lg">731 155 158</span>
-            </motion.a>
-          </div>
-        </div>
-      </div>
-    </motion.div>
-  )}
-</AnimatePresence>
-
+								<div className="mx-auto mt-4 w-full">
+									<motion.a
+										initial={{ opacity: 0, y: 30 }}
+										animate={{ opacity: 1, y: 0 }}
+										transition={{ duration: 0.5, delay: 1 }}
+										whileHover={{ scale: 1.1, transition: { duration: 0.3 } }}
+										href={`tel:${CISLO}`}
+										className="flex w-full items-center justify-center rounded-lg border-2 border-[#e94629] bg-black/70 p-1 text-[#e94629] transition-none focus:duration-300 focus:transition-transform focus:scale-110"
+									>
+										<IoCallOutline className="p-1 text-4xl" />
+										<span className="ml-2 text-lg">731 155 158</span>
+									</motion.a>
+								</div>
+							</div>
+						</div>
+					</motion.div>
+				)}
+			</AnimatePresence>
 		</>
 	);
 }
